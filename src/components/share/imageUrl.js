ed53e8711efb4b20ba0afe207ea9ac66a2cd9815
MdJowelAhmed <mdjowelahmed924@gmail.com>
2025-12-14 14:41:44 +0600

    Baseurl changed

--- src/components/share/imageUrl.js
+++ src/components/share/imageUrl.js
@@ -6,7 +6,7 @@ export const getImageUrl = (path) => {
   if (path.startsWith("http://") || path.startsWith("https://")) {
     return path;
   } else {
-    const baseUrl = "https://ftp.thepigeonhub.com";
+    const baseUrl = "https://api.thepigeonhub.com";
     // const baseUrl = "http://50.6.200.33:5001";
     // const baseUrl = "http://10.10.7.41:5001";
     return `${baseUrl}/${path}`;
--- src/redux/baseUrl/baseUrl.js
+++ src/redux/baseUrl/baseUrl.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://ftp.thepigeonhub.com/api/v1",
+    baseUrl: "https://api.thepigeonhub.com/api/v1",
     // baseUrl: "http://50.6.200.33:5001/api/v1",
     // baseUrl: "http://10.10.7.41:5001/api/v1",
     prepareHeaders: (headers) => {
